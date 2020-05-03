1b2d3d981e775fa3586984aae5c0c72162d60f26
Julia K <julia326@gmail.com>
2020-05-03 17:42:02 -0400

    Fixing Markupsafe version to make CircleCI happy (see https://github.com/freedomofpress/securedrop/issues/5150)

--- app/models/data.py
+++ app/models/data.py
@@ -26,7 +26,7 @@ class Batch(db.Model):
         super(Batch, self).__init__(**kwargs)
 
     def to_dict(self):
-        out = {}
+        d = {}
         for column in self.__table__.columns:
             d[column.name] = str(getattr(self, column.name))
         return d

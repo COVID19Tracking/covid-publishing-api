391b0261fb5219361e5d509e8ae1cf611f5ab060
Alex Silverstein <aslvrstn@gmail.com>
2020-05-03 22:04:47 -0400

    Move some reqs to setup.py

--- data_convert/setup.py
+++ data_convert/setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 
 setup(
     name='data_convert',
-    version='0.1'
+    version='0.1',
+    install_requires=[
+        'loguru',
+        'cryptography',
+        'pandas',
+        'numpy'
+    ]
 )

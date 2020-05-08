0fa3cde11fd62391a27f376652727773836e3c92
Alex Silverstein <aslvrstn@gmail.com>
2020-05-07 22:04:51 -0400

    Add an example test for hitting the /batch/ endpoint (#32)
    * WIP: POC of injecting straight into the db
    
    * Make it a bit clearer
    
    * Comment
    
    * resp.json

--- tests/app/v1_basic_test.py
+++ tests/app/v1_basic_test.py
@@ -5,7 +5,11 @@ import pytest
 
 from flask import json, jsonify
 
-def test_get_test(client):
+from app import db
+from app.models.data import *
+
+def test_get_test(app):
+    client = app.test_client()
     resp = client.get("/api/v1/test/")
     assert resp.data != None 
     assert resp.status_code == 200
@@ -13,7 +17,8 @@ def test_get_test(client):
     assert "test_data_key" in data 
     assert data["test_data_key"] == "test_data_value" 
 
-def test_post(client):
+def test_post(app):
+    client = app.test_client()
     payload = {"testy": "mctest"}
     resp = client.post(
         "/api/v1/data/batch/",
@@ -29,3 +34,14 @@ def test_post(client):
         data=json.dumps(payload),
         content_type='application/json')
     assert resp.status_code == 400
+
+def test_models(app):
+    # `db` is only valid within an app context, so start one and then shove some test data into it.
+    with app.app_context():
+        bat = Batch(batch_note='test', created_at=datetime.now(), is_published=False, is_revision=False)
+        db.session.add(bat)
+        db.session.commit()
+
+    client = app.test_client()
+    resp = client.get("/api/v1/data/batch/")
+    assert "batches" in resp.json

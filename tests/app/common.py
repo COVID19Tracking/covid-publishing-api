3b32708864af7bc6b3875369bd41141dbfe5c9f3
Julia K <julia326@gmail.com>
2021-03-05 13:04:49 -0500

    Default to not serving data for dates past 3/7/2021.

--- tests/app/common.py
+++ tests/app/common.py
@@ -5,6 +5,8 @@ BEFORE_YESTERDAY = date(2020, 5, 20)
 YESTERDAY = date(2020, 5, 24)
 TODAY = date(2020, 5, 25)
 NOW = pytz.utc.localize(datetime.now())
+MARCH7 = date(2021, 3, 7)
+MARCH8 = date(2021, 3, 8)
 
 NY = {"state": "NY", "totalTestResultsFieldDbColumn": "posNeg"}
 CA_DIFFERENT_TOTAL_SOURCE = {"state": "CA", "totalTestResultsFieldDbColumn": "totalTestsViral"}
@@ -20,15 +22,6 @@ NY_TODAY = {
     "inIcuCurrently": 33
 }
 
-WA_TODAY = {
-    "state": "WA",
-    "lastUpdateIsoUtc": NOW.isoformat(),
-    "dateChecked": NOW.isoformat(),
-    "date": TODAY,
-    "positive": 10,
-    "negative": 10
-}
-
 NY_YESTERDAY = {
     "state": "NY",
     "lastUpdateIsoUtc": NOW.isoformat(),
@@ -39,6 +32,35 @@ NY_YESTERDAY = {
     "inIcuCurrently": 37
 }
 
+NY_MARCH7 = {
+    "state": "NY",
+    "lastUpdateIsoUtc": NOW.isoformat(),
+    "dateChecked": NOW.isoformat(),
+    "date": MARCH7,
+    "positive": 15,
+    "negative": 4,
+    "inIcuCurrently": 37
+}
+
+NY_MARCH8 = {
+    "state": "NY",
+    "lastUpdateIsoUtc": NOW.isoformat(),
+    "dateChecked": NOW.isoformat(),
+    "date": MARCH8,
+    "positive": 16,
+    "negative": 4,
+    "inIcuCurrently": 35
+}
+
+WA_TODAY = {
+    "state": "WA",
+    "lastUpdateIsoUtc": NOW.isoformat(),
+    "dateChecked": NOW.isoformat(),
+    "date": TODAY,
+    "positive": 10,
+    "negative": 10
+}
+
 WA_YESTERDAY = {
     "state": "WA",
     "lastUpdateIsoUtc": NOW.isoformat(),
@@ -46,6 +68,24 @@ WA_YESTERDAY = {
     "date": YESTERDAY,
     "positive": 9,
     "negative": 8
+}
+
+WA_MARCH7 = {
+    "state": "WA",
+    "lastUpdateIsoUtc": NOW.isoformat(),
+    "dateChecked": NOW.isoformat(),
+    "date": MARCH7,
+    "positive": 10,
+    "negative": 10
+}
+
+WA_MARCH8 = {
+    "state": "WA",
+    "lastUpdateIsoUtc": NOW.isoformat(),
+    "dateChecked": NOW.isoformat(),
+    "date": MARCH8,
+    "positive": 11,
+    "negative": 11
 }
 
 # edit to increase positive count by 1
@@ -152,6 +192,19 @@ def daily_push_ny_wa_today():
       "context": ctx,
       "states": [NY, WA],
       "coreData": [NY_TODAY.copy(), WA_TODAY.copy()]
+    }
+
+def daily_push_ny_wa_march_2021():
+    ctx = {
+      "dataEntryType": "daily",
+      "shiftLead": "test",
+      "batchNote": "This is a future test"
+    }
+
+    return {
+      "context": ctx,
+      "states": [NY, WA],
+      "coreData": [NY_MARCH7.copy(), NY_MARCH8.copy(), WA_MARCH7.copy(), WA_MARCH8.copy()]
     }
 
 def edit_push_ny_yesterday():
